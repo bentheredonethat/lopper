7daf51d2c40aa4436c80ef6bf2d387c383faedc1
Ben Levinsky <ben.levinsky@amd.com>
2023-06-01 15:26:02 -0700

    openamp: xlnx: Update plugin to generate header based on 'role' CLI option
    If the option 'role' is passed with either 'host' or 'remote' as input
    then the OpenAMP Xilinx plugin will output a header that can be passed
    into the OpenAMP repo to configure the application.
    
    Signed-off-by: Ben Levinsky <ben.levinsky@amd.com>

--- lopper/assists/openamp_xlnx_common.py
+++ lopper/assists/openamp_xlnx_common.py
@@ -411,3 +411,122 @@ def resolve_host_remote( tree, subnode, verbose = 0 ):
     if domain_node.props("phandle") == []:
         domain_node + LopperProp(name="phandle", value=domain_node.phandle)
     return True
+
+platform_info_header_template = """
+/*
+ * Copyright (c) 2014, Mentor Graphics Corporation
+ * All rights reserved.
+ * Copyright (c) 2017 Xilinx, Inc.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef PLATFORM_INFO_H_
+#define PLATFORM_INFO_H_
+
+#include <openamp/remoteproc.h>
+#include <openamp/virtio.h>
+#include <openamp/rpmsg.h>
+#include \"xparameters.h\"
+
+#if defined __cplusplus
+extern \"C\" {
+#endif
+
+/* Cortex R5 memory attributes */
+#define DEVICE_SHARED       0x00000001U /* device, shareable */
+#define DEVICE_NONSHARED    0x00000010U /* device, non shareable */
+#define NORM_NSHARED_NCACHE 0x00000008U /* Non cacheable  non shareable */
+#define NORM_SHARED_NCACHE  0x0000000CU /* Non cacheable shareable */
+#define PRIV_RW_USER_RW     (0x00000003U<<8U) /* Full Access */
+
+/* Interrupt vectors */
+#define IPI_IRQ_VECT_ID		$IPI_IRQ_VECT_ID
+#define POLL_BASE_ADDR		$POLL_BASE_ADDR
+#define IPI_CHN_BITMASK		$IPI_CHN_BITMASK
+
+#define NUM_VRINGS                  0x02
+#define VRING_ALIGN                 0x1000
+#define VRING_SIZE                  256
+
+#define RING_TX			$RING_TX
+#define RING_RX			$RING_RX
+
+#define SHARED_MEM_PA		$SHARED_MEM_PA
+#define SHARED_MEM_SIZE		$SHARED_MEM_SIZE
+#define SHARED_BUF_OFFSET	$SHARED_BUF_OFFSET
+
+struct remoteproc_priv {
+	const char *kick_dev_name;
+	const char *kick_dev_bus_name;
+	struct metal_device *kick_dev;
+	struct metal_io_region *kick_io;
+#ifndef RPMSG_NO_IPI
+	unsigned int ipi_chn_mask; /**< IPI channel mask */
+	atomic_int ipi_nokick;
+#endif /* !RPMSG_NO_IPI */
+};
+
+/**
+ * platform_init - initialize the platform
+ *
+ * It will initialize the platform.
+ *
+ * @argc: number of arguments
+ * @argv: array of the input arguments
+ * @platform: pointer to store the platform data pointer
+ *
+ * return 0 for success or negative value for failure
+ */
+int platform_init(int argc, char *argv[], void **platform);
+
+/**
+ * platform_create_rpmsg_vdev - create rpmsg vdev
+ *
+ * It will create rpmsg virtio device, and returns the rpmsg virtio
+ * device pointer.
+ *
+ * @platform: pointer to the private data
+ * @vdev_index: index of the virtio device, there can more than one vdev
+ *              on the platform.
+ * @role: virtio driver or virtio device of the vdev
+ * @rst_cb: virtio device reset callback
+ * @ns_bind_cb: rpmsg name service bind callback
+ *
+ * return pointer to the rpmsg virtio device
+ */
+struct rpmsg_device *
+platform_create_rpmsg_vdev(void *platform, unsigned int vdev_index,
+			   unsigned int role,
+			   void (*rst_cb)(struct virtio_device *vdev),
+			   rpmsg_ns_bind_cb ns_bind_cb);
+
+/**
+ * platform_poll - platform poll function
+ *
+ * @platform: pointer to the platform
+ *
+ * return negative value for errors, otherwise 0.
+ */
+int platform_poll(void *platform);
+
+/**
+ * platform_release_rpmsg_vdev - release rpmsg virtio device
+ *
+ * @rpdev: pointer to the rpmsg device
+ */
+void platform_release_rpmsg_vdev(struct rpmsg_device *rpdev, void *platform);
+
+/**
+ * platform_cleanup - clean up the platform resource
+ *
+ * @platform: pointer to the platform
+ */
+void platform_cleanup(void *platform);
+
+#if defined __cplusplus
+}
+#endif
+
+#endif /* PLATFORM_INFO_H_ */
+"""
